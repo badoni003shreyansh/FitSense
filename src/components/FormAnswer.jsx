ccec64252025cd4440e0aac8c7727fb5789587c2
BadoniShreyansh <shreyansh.badoni@writesonic.com>
2025-06-02 00:55:22 +0530

    fourth-commit

--- src/components/FormAnswer.jsx
+++ src/components/FormAnswer.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
+const MarkComponent = ({ value }) => {
+  return { value };
+};
 function FormAnswer({ answers }) {
   return (
     <div className="flex flex-col max-w-5xl w-full card rounded-2xl shadow-lg p-6 bg-white">
       <p className="text-base leading-relaxed text-gray-800 text-wrap">
-        <ReactMarkdown>{answers}</ReactMarkdown>
+        <ReactMarkdown
+          components={{
+            code: MarkComponent,
+          }}
+        >
+          {answers}
+        </ReactMarkdown>
       </p>
     </div>
   );
